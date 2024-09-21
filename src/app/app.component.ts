f9a42f9b04e789872fabf738e75048c96bb89c05
PeddarmajiSaikumar <saikumarpeddarmaji@gmail.com>
2024-09-21 09:59:26 +0530

    Nav-Bar

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TopNavComponent } from "./top-nav/top-nav.component";
+import { SideNavComponent } from "./side-nav/side-nav.component";
+import { FooterComponent } from "./footer/footer.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, TopNavComponent, SideNavComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'my-app';
+}
